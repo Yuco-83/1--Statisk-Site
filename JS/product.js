91e69c682ea9728f690b1a44490a007ef0d55b84
Yuleisy C. Rojas <yolocr@yahoo.dk>
2025-01-31 00:20:07 +0100

    udkomentere

--- JS/product.js
+++ JS/product.js
@@ -1,9 +1,9 @@
-let productID = 1165;
+// let productID = 1165;
 
-let productContainer = document.querySelector(".productContainer");
-fetch("https://kea-alt-del.dk/t7/api/products/1165")
-     .then((response) => response.json())
-     .then((data) => {
-          productContainer.innerHTML = `
+// let productContainer = document.querySelector(".productContainer");
+// fetch("https://kea-alt-del.dk/t7/api/products/1165")
+//      .then((response) => response.json())
+//      .then((data) => {
+//           productContainer.innerHTML = `
 
-     });
+//      });
