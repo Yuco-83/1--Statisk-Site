63609bfce5976ba6f8efff3c80b743044178c419
Yuleisy C. Rojas <yolocr@yahoo.dk>
2025-01-31 11:06:11 +0100

    JS.mapOgJoin

--- JS/list.js
+++ JS/list.js
@@ -1,5 +1,5 @@
-const productId = 1530;
-const productContainer = document.querySelector(".productContainer");
+const productId = 1163;
+const productListContainer = document.querySelector(".product-list-container");
 
 fetch(`https://kea-alt-del.dk/t7/api/products/`)
 	.then((response) => response.json())
@@ -10,18 +10,19 @@ fetch(`https://kea-alt-del.dk/t7/api/products/`)
 		let markup = "";
 		products
 			.map((product) => {
-				markup += `<article class="product">
+				markup += `<article class="product onSale">
+                    <div class="sold-out">${product.soldout}</div>
 				<img src="./IMAGES/Product images/1163.webp" alt="Sahara Team India Fanwear Round Neck Jersey" />
-				<h6 class="product_name">Sahara Team India Fanwear Round Neck Jersey</h6>
+				<h6 class="product_name">${product.productdisplayname}</h6>
 				<div class="p-categories">
-					<p class="type">Tshirts</p>
+					<p class="type">${product.articletype}</p>
 					<p class="separator">|</p>
-					<p class="brand">Nike</p>
+					<p class="brand">${product.brandname}</p>
 				</div>
 				<div class="price">
-					<p class="price">1595,-</p>
+					<p class="price">${product.price}</p>
 					<div class="discount">
-						<p class="discount_price">DKK 1595</p>
+						<p class="discount_price">${product.discount}</p>
 						<p class="discount-percent">34%</p>
 					</div>
 				</div>
@@ -32,7 +33,7 @@ fetch(`https://kea-alt-del.dk/t7/api/products/`)
 			})
 			.join("");
 		console.log(markup);
-		listOContainer.innerHTML = markup;
+		productListContainer.innerHTML = markup;
 	}
 	// productContainer.innerHTML = `JSON kode her`;
 }
